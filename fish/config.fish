f5eed717cff7efa7aec8f617a00226982d39f977
audns <audns@tuta.io>
2025-11-05 15:37:34 +0800

    commit

--- fish/config.fish
+++ fish/config.fish
@@ -8,12 +8,9 @@ set -U fish_greeting ""
 starship init fish | source
 
 # Alias
-alias cd='z'
 alias pparu='proxychains4 -q paru'
 alias vmrestart='sudo modprobe -v vmmon; sudo modprobe vmnet; sudo systemctl start vmware-networks.service; sudo systemctl start vmtoolsd.service'
-alias cat='bat'
 alias ls='eza --color=always --icons=always --group-directories-first'
 alias lla='eza --color=always --icons=always -al --group-directories-first'
 alias ip='ip -c'
-alias grep='rg'
 alias hx='helix'
