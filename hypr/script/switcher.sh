2c6362ead2609ae6949a323028a38c8a31687f53
audns <audns@tuta.io>
2025-12-14 18:52:54 +0800

    commit

--- hypr/script/switcher.sh
+++ hypr/script/switcher.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 window_list=$(hyprctl clients -j | jq -r '.[] | "\(.workspace.id)|\(.title)|\(.class)"' | sort -n)
 
 if [ -z "$window_list" ]; then
@@ -8,13 +7,11 @@ if [ -z "$window_list" ]; then
 fi
 
 formatted_list=$(echo "$window_list" | awk -F'|' '{printf "%s: %s - %s\n", $1, $3, $2}')
-
 selected=$(echo "$formatted_list" | fuzzel --dmenu --prompt="Window>")
 
 if [ -z "$selected" ]; then
 	exit 0
 fi
 
-workspace_id=$(echo "$selected" | sed 's/WS:\([0-9]*\).*/\1/')
-
+workspace_id=$(echo "$selected" | sed 's/^\([0-9]*\).*/\1/')
 hyprctl dispatch workspace "$workspace_id"
