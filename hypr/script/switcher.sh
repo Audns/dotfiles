ca88325883b8992ad80c1254ff9d757ceca715ef
audns <audns@tuta.io>
2025-12-22 17:20:01 +0800

    commit

--- hypr/script/switcher.sh
+++ hypr/script/switcher.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-window_list=$(hyprctl clients -j | jq -r '.[] | "\(.workspace.id)|\(.title)|\(.class)"' | sort -n)
+
+# Use a unique delimiter that won't appear in window titles
+DELIM=$'\x1F' # ASCII Unit Separator character
+
+window_list=$(hyprctl clients -j | jq -r --arg delim "$DELIM" '.[] | "\(.workspace.id)\($delim)\(.title)\($delim)\(.class)"' | sort -n)
 
 if [ -z "$window_list" ]; then
 	notify-send "No windows found"
 	exit 1
 fi
 
-formatted_list=$(echo "$window_list" | awk -F'|' '{printf "%s: %s - %s\n", $1, $3, $2}')
+formatted_list=$(echo "$window_list" | awk -F"$DELIM" '{printf "%s: %s - %s\n", $1, $3, $2}')
+
 selected=$(echo "$formatted_list" | fuzzel --dmenu --prompt="Window>")
 
 if [ -z "$selected" ]; then
@@ -14,4 +19,5 @@ if [ -z "$selected" ]; then
 fi
 
 workspace_id=$(echo "$selected" | sed 's/^\([0-9]*\).*/\1/')
+
 hyprctl dispatch workspace "$workspace_id"
