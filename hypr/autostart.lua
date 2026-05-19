ec76ca727dce9e2dbe5ea99687d2fc717f148538
audns <audns@tuta.io>
2026-05-19 12:04:07 +0800

    commit

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -3,7 +3,7 @@
 hl.on("hyprland.start", function()
   hl.exec_cmd("hyprpaper")
   hl.exec_cmd("~/second/codespace/simple_project/Waybar/build/waybar")
-  hl.exec_cmd("rustbox activate -i \"4ef5c9cd-72f6-40d1-8048-7db76eb5882a\" -e 19")
+  hl.exec_cmd("rustbox start -i \"4ef5c9cd-72f6-40d1-8048-7db76eb5882a\" -e 19")
   hl.exec_cmd("mako")
   hl.exec_cmd("fcitx5")
   hl.exec_cmd("luncher --daemon")
